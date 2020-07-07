d0ff39c44204120a1cdb8ec167e4aec9be1a54e5
Antonio Filipo de Mozer Namur <61598094+magemongo@users.noreply.github.com>
2020-07-07 15:45:50 -0300

    Update awis.py

--- python/awis.py
+++ python/awis.py
@@ -312,7 +312,7 @@ if __name__ == "__main__":
     
     print('\nRESPONSE++++++++++++++++++++++++++++++++++++')
     print('Response code: %d\n' % r.status_code)
-    print(r.text)
+    #print(r.text)
     data = r.text
     df = pd.read_json(data, orient='records')
-    df
+    df.head()
